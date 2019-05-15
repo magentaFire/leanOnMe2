7c7113ffcc9cb281eb770f540d1f6526d696da74
magentaFire <dafne.magenta@gmail.com>
2019-05-15 18:25:41 -0400

    wip description

---
+++ src/components/Description.vue
@@ -0,0 +1,43 @@
+<template class="login">
+  <div>
+    <section>
+      <h4>
+        To begin we gather information about your idea.
+      </h4>
+      <div class="form">
+        <input type="text" placeholder="Project name" />
+        <input type="text" placeholder="Logo" />
+        <input type="text" placeholder="Category" />
+        <input type="text" placeholder="Tags" />
+        <input type="text" placeholder="Description" />
+        <input type="text" placeholder="Invite friends" />
+      </div>
+      <router-link :to="{ name: 'canvas' }" class="contenBtn">
+        <button>
+          Save
+        </button>
+      </router-link>
+    </section>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      obj: {
+        bla: "algo",
+        otrobla: []
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+@import "../assets/style.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "@/components/Login";
 import Welcome from "@/components/Welcome";
+import Description from "@/components/Description";
 
 Vue.use(Router);
 
@@ -14,8 +15,13 @@ export default new Router({
     },
     {
       path: "/welcome",
-      name: "Welcome",
+      name: "welcome",
       component: Welcome
+    },
+    {
+      path: "/description",
+      name: "description",
+      component: Description
     }
   ]
 });
