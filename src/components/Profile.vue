2cf790ba881f76ba822b275f8e3631e0552ce3de
Dafne Magenta Luna <dafne.l@adext.com>
2019-05-15 18:17:26 -0400

    Adds Profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="@/assets/magenta.jpg" alt="" class="mb-3" />
+    </div>
+    <div>
+      <div class="form">
+        <input class="mb-1" type="text" name="name" placeholder="Name" />
+        <input
+          class="mb-1"
+          type="text"
+          name="location"
+          placeholder="Location"
+        />
+        <input class="mb-1" type="email" name="email" placeholder="Email" />
+        <input class="mb-1" type="text" name="phone" placeholder="Phone" />
+        <input class="mb-1" type="text" name="Age" placeholder="Age" />
+        <input class="mb-1" type="text" name="Gender" placeholder="Gender" />
+      </div>
+      <router-link :to="{ name: 'description' }" class="contenBtn">
+        <button>
+          Save
+        </button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../assets/style.scss";
+img {
+  max-width: 60vw;
+  transform: rotate(-90deg);
+}
+</style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="welcome-page">
-    <h2>Welcome!</h2>
-    <i class="fas fa-coffee"></i>
-    <p class="shift-2">
+  <div class="container">
+    <h2 class="mb-1">Welcome!</h2>
+    <i class="fas fa-coffee mb-1"></i>
+    <p class="mb-3">
       Take the first steps to thrive! Congratulations on joning our women
       oriented business community, we call our menbers LEANies. Mentors and
       investorsare LEANers for us We hope to help you build your new company
@@ -10,48 +10,38 @@
       the business model a more inclusive place.
     </p>
     <div class="steps">
-      <h3>Step 1</h3>
-      <h5>
+      <router-link :to="{ name: 'leanie-profile' }" class="link">
+        <h3>Step 1</h3>
+      </router-link>
+      <h5 class="mb-1">
         Tell us about you!
-        <i
-          class="fas fa-info-circle"
-          @click="profileInfo = !profileInfo"
-        ></i>
+        <i class="fas fa-info-circle" @click="profileInfo = !profileInfo"></i>
       </h5>
       <p v-if="profileInfo">
         We love to know about you, please tell us who you are.
       </p>
       <h3>Step 2</h3>
-      <h5>
+      <h5 class="mb-1">
         Project description
-        <i
-          class="fas fa-info-circle"
-          @click="projectInfo = !projectInfo"
-        ></i>
+        <i class="fas fa-info-circle" @click="projectInfo = !projectInfo"></i>
       </h5>
       <p v-if="projectInfo">
         What's your project about? we are thrilled about helping you building
         your new empire.
       </p>
       <h3>Step 3</h3>
-      <h5>
+      <h5 class="mb-1">
         Build your lean canvas
-        <i
-          class="fas fa-info-circle"
-          @click="canvasInfo = !canvasInfo"
-        ></i>
+        <i class="fas fa-info-circle" @click="canvasInfo = !canvasInfo"></i>
       </h5>
       <p v-if="canvasInfo">
         This tool will help you to build a business model you can share with
         LEANers or the LEANies community
       </p>
       <h3>Step 4</h3>
-      <h5>
+      <h5 class="mb-1">
         Metrics
-        <i
-          class="fas fa-info-circle"
-          @click="metricsInfo = !metricsInfo"
-        ></i>
+        <i class="fas fa-info-circle" @click="metricsInfo = !metricsInfo"></i>
       </h5>
       <p v-if="metricsInfo">
         Feed our calculator with your company metrics and thrive with help of
@@ -77,14 +67,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.welcome-page {
-  padding: 0 1.5rem;
-  margin-bottom: 2.5rem;
-}
-.shift-2 {
-  margin-bottom: 2rem;
-}
+<style lang="scss" scoped>
+@import "../assets/style.scss";
+
 .steps {
   display: block;
   text-align: left;
