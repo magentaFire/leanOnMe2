7c20333d39ead3c4fcc15b2f73e9973df604114c
Dafne Magenta Luna <dafne.l@adext.com>
2019-05-15 15:21:49 -0400

    Adds welcome page and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
+
+library.add(faUserSecret);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
