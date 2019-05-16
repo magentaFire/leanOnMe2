2c8edc42696f19778b3ab481ba3cf75720e89e9c
magentaFire <dafne.magenta@gmail.com>
2019-05-15 21:09:58 -0400

    style inputs

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,9 +1,10 @@
 <template class="login">
   <div>
-    <section>
-      <h4>
-        To begin we gather information about your idea.
-      </h4>
+    <section class="container">
+      <h3>
+        <i class="fas fa-lightbulb"></i> To begin we gather information about
+        your idea.
+      </h3>
       <div class="form">
         <input type="text" placeholder="Project name" />
         <input type="text" placeholder="Logo" />
@@ -13,9 +14,11 @@
         <input type="text" placeholder="Invite friends" />
       </div>
       <router-link :to="{ name: 'canvas' }" class="contenBtn">
-        <button>
-          Save
-        </button>
+        <div class="contentBtn">
+          <button>
+            Save
+          </button>
+        </div>
       </router-link>
     </section>
     <router-view />
